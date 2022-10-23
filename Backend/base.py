74a823d4584471c390ed82ef53c9a4aa962be343
SinlessMirror <loganpederson@gmail.com>
2022-10-23 16:07:12 -0500

    pool_timeout=False for postgres engine

--- Backend/base.py
+++ Backend/base.py
@@ -10,7 +10,7 @@ def get_engine(user, passwd, host, port, db):
     if not database_exists(url):
         print("database does not exist?!!")
         create_database(url)
-    engine = create_engine(url, pool_size=50, echo=False) # echo=True for verbose
+    engine = create_engine(url, pool_size=50, echo=False, pool_timeout=False) # echo=True for verbose
     return engine
 
 engine = get_engine(
